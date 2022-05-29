fcfcc02b4cc2310f40c676eb1683f8850ccfd9fa
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-29 18:20:09 +0200

    Rewrite arc entity to center_point

--- AC1003.pm
+++ AC1003.pm
@@ -3768,8 +3768,7 @@ sub _read {
     my ($self) = @_;
 
     $self->{entity_common} = CAD::Format::DWG::AC1003::EntityCommon->new($self->{_io}, $self, $self->{_root});
-    $self->{x} = $self->{_io}->read_f8le();
-    $self->{y} = $self->{_io}->read_f8le();
+    $self->{center_point} = CAD::Format::DWG::AC1003::Point2d->new($self->{_io}, $self, $self->{_root});
     $self->{radius} = $self->{_io}->read_f8le();
     $self->{angle_from} = $self->{_io}->read_f8le();
     $self->{angle_to} = $self->{_io}->read_f8le();
@@ -3780,14 +3779,9 @@ sub entity_common {
     return $self->{entity_common};
 }
 
-sub x {
+sub center_point {
     my ($self) = @_;
-    return $self->{x};
-}
-
-sub y {
-    my ($self) = @_;
-    return $self->{y};
+    return $self->{center_point};
 }
 
 sub radius {
